4cd0081b277a022a50465c79ca19d8d66acee29d
David Lukes <dafydd.lukes@gmail.com>
2017-01-18 23:53:55 +0100

    comment and tinker with code w/o breaking stuff

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import {
+  VDomModel
+} from "jupyterlab/lib/common/vdom";
+
+export
+  interface IClash { }
+
+export
+class ClashModel extends VDomModel implements IClash {}
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,16 +1,27 @@
 import {
-  JupyterLabPlugin
+  JupyterLab, JupyterLabPlugin
 } from 'jupyterlab/lib/application';
+
+import {
+  IClash, ClashModel
+} from "./";
 
 /**
  * Initialization data for the jupyterlab_clash extension.
  */
-const extension: JupyterLabPlugin<void> = {
+// instead of <void>, this should be a type parameter corresponding to the type of my extension
+// widget, as defined in an index.ts in this directory
+const extension: JupyterLabPlugin<IClash> = {
   id: 'jupyterlab_clash',
+  // stuff that's required here will be passed as additional arguments to activate below
+  // requires: [IServiceManager, IPathTracker, ...]
   autoStart: true,
-  activate: (app) => {
-    console.log('JupyterLab extension jupyterlab_clash is activated!');
-  }
+  activate
 };
 
 export default extension;
+
+function activate(app: JupyterLab): IClash {
+  console.log('JupyterLab extension jupyterlab_clash is activated!');
+  return new ClashModel;
+}
