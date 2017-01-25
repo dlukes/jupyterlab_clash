40bbe3c83079181ad784c9d6c1f5ebadfced5396
David Lukes <dafydd.lukes@gmail.com>
2017-01-25 18:07:03 +0100

    create a GUI tab for Clash on plugin startup
    - also, clean up semi-colons in .ts sources

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,35 @@
 import {
   VDomModel
 } from "jupyterlab/lib/common/vdom";
+import { Widget } from 'phosphor/lib/ui/widget';
 
 export
-  interface IClash { }
+interface IClash { }
 
 export
 class ClashModel extends VDomModel implements IClash {}
+
+export class ClashWidget extends Widget {
+
+  constructor() {
+    super()
+    this.id = "jupyterlab_clash_unique_id"
+    this.title.label = "Clash"
+    this.title.closable = true
+  }
+
+  // TODO: import Message and specify type of msg
+  onAfterAttach(msg: any) {
+    this._render()
+  }
+
+  // TODO: import Message and specify type of msg
+  onBeforeDetach(msg: any) {
+    delete this.node
+  }
+
+  _render() {
+    this.node.innerHTML = `<h3>Corpus Linguistics Advanced Shell</h3>
+      <p>Hello, world of corpus linguistics, and prepare to be amazed!</p>`
+  }
+}
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,27 +1,29 @@
 import {
   JupyterLab, JupyterLabPlugin
-} from 'jupyterlab/lib/application';
+} from 'jupyterlab/lib/application'
 
 import {
-  IClash, ClashModel
-} from "./";
+  IClash, ClashModel, ClashWidget
+} from "."
 
 /**
  * Initialization data for the jupyterlab_clash extension.
  */
-// instead of <void>, this should be a type parameter corresponding to the type of my extension
-// widget, as defined in an index.ts in this directory
+// JupyterLabPlugin should have a type parameter corresponding to the type of my extension widget,
+// as defined in an index.ts in this directory
 const extension: JupyterLabPlugin<IClash> = {
   id: 'jupyterlab_clash',
   // stuff that's required here will be passed as additional arguments to activate below
   // requires: [IServiceManager, IPathTracker, ...]
   autoStart: true,
   activate
-};
+}
 
-export default extension;
+export default extension
 
 function activate(app: JupyterLab): IClash {
-  console.log('JupyterLab extension jupyterlab_clash is activated!');
-  return new ClashModel;
+  console.log('JupyterLab extension jupyterlab_clash is activated!')
+  let widget = new ClashWidget
+  app.shell.addToMainArea(widget)
+  return new ClashModel
 }
