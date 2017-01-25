090a78bd588256e1ceb29c8c7d34654a4a9b27cc
David Lukes <dafydd.lukes@gmail.com>
2017-01-25 18:37:35 +0100

    add "boilerplate"
    - taking inspiration from the launcher plugin, add stuff like a plugin Token that wasn't strictly
      necessary for the plugin to start to work, but probably should be there
    - more consistent formatting (removed semicolons, unified quotes to double)

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
-import {
-  VDomModel
-} from "jupyterlab/lib/common/vdom";
-import { Widget } from 'phosphor/lib/ui/widget';
+import { VDomModel } from "jupyterlab/lib/common/vdom"
+import { Widget } from "phosphor/lib/ui/widget"
+import { Token } from "phosphor/lib/core/token"
 
 export
 interface IClash { }
 
 export
-class ClashModel extends VDomModel implements IClash {}
+const IClash = new Token<IClash>("cz.korpus.jupyter.extensions.clash")
+
+export
+class ClashModel extends VDomModel implements IClash { }
 
 export class ClashWidget extends Widget {
+  model: ClashModel
 
   constructor() {
     super()
+    // NOTE: in the JupyterLab codebase (e.g. in the launcher plugin.ts), I've seen places where the
+    // model is created separately and then slapped onto the widget -- perhaps there's a good reason
+    // for that?
+    this.model = new ClashModel()
     this.id = "jupyterlab_clash_unique_id"
     this.title.label = "Clash"
     this.title.closable = true
@@ -28,7 +35,7 @@ export class ClashWidget extends Widget {
     delete this.node
   }
 
-  _render() {
+  private _render() {
     this.node.innerHTML = `<h3>Corpus Linguistics Advanced Shell</h3>
       <p>Hello, world of corpus linguistics, and prepare to be amazed!</p>`
   }
