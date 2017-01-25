804a90f56f7567acc4b2a7073aafbc1274ec3843
David Lukes <dafydd.lukes@gmail.com>
2017-01-25 18:19:58 +0100

    update extension id

--- src/plugin.ts
+++ src/plugin.ts
@@ -12,7 +12,7 @@ import {
 // JupyterLabPlugin should have a type parameter corresponding to the type of my extension widget,
 // as defined in an index.ts in this directory
 const extension: JupyterLabPlugin<IClash> = {
-  id: 'jupyterlab_clash',
+  id: 'cz.korpus.jupyter.extensions.clash',
   // stuff that's required here will be passed as additional arguments to activate below
   // requires: [IServiceManager, IPathTracker, ...]
   autoStart: true,
